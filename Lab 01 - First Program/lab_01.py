1b4c3d089e96440b3ccf94c03776cf372f220ba4
Brannon Corden <BCorden@stu.kerikerihigh.ac.nz>
2021-03-30 12:15:03 +1300

    added background and circle

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,18 @@
+import arcade
+
+arcade.open_window(600, 600, "My first arcade game")
+arcade.set_background_color(arcade.color.AMBER)
+arcade.start_render()
+
+
+
+
+
+arcade.draw_circle_filled(300, 300, 40, arcade.color.BARBIE_PINK)
+
+
+
+
+arcade.finish_render()
+
+arcade.run()
